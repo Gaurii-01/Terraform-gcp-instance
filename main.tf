a9f3380899072106768553c32acb3e66da0681bb
Gauri Kulkarni <142674318+Gaurii-01@users.noreply.github.com>
2025-12-09 17:26:28 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+resource "google_compute_instance" "vm_instance" {
+  name         = var.instance_name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "${var.image_project}/${var.image_family}"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}  # Assigns a public IP
+  }
+
+  tags = ["web", "dev"]
+
+  metadata = {
+    ssh-keys = "jenkins:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
