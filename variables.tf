ae94d644849a5625502810a7612222a3fd13fc26
Gauri Kulkarni <142674318+Gaurii-01@users.noreply.github.com>
2025-12-09 17:26:01 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,37 @@
+variable "project" {
+  description = "GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+}
+
+variable "zone" {
+  description = "GCP zone"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "Name of the VM instance"
+  type        = string
+}
+
+variable "machine_type" {
+  description = "Machine type for the VM"
+  type        = string
+  default     = "e2-medium"
+}
+
+variable "image_family" {
+  description = "Image family for the VM"
+  type        = string
+  default     = "debian-11"
+}
+
+variable "image_project" {
+  description = "Project containing the image"
+  type        = string
+  default     = "debian-cloud"
+}
